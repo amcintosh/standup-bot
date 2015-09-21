a921bd4f7d14be98c7a960b8c5cbc3ca84e50591
Andrew McIntosh <amcintosh@freshbooks.com>
2015-09-20 22:07:42 -0400

    Some initial logic

--- standup-bot.py
+++ standup-bot.py
@@ -1,17 +1,81 @@
 from __future__ import print_function
+import os
+import random
+import re
 from flask import Flask, request
+from imgurpython import ImgurClient
+from Slacker import Slacker
 
 app = Flask(__name__)
+
+slack_client = Slacker(os.getenv('SLACK_TOKEN'))
+slack_username = os.getenv('SLACK_USERNAME', 'Standup-bot')
+slack_channel = os.getenv('SLACK_CHANNEL')
+slack_message = os.getenv("SLACK_MESSAGE", "Shall we standup?")
+
+imgur_album_id = os.getenv("IMGUR_ALBUM_ID")
+imgur_client = ImgurClient(
+    os.getenv("IMGUR_CLIENT_ID"),
+    os.getenv("IMGUR_CLIENT_SECRET")
+)
+
+
+def post_message(text, attachments=[]):
+    slack_client.chat.post_message(
+        channel=slack_channel,
+        text=text,
+        username=slack_username,
+        parse="full",
+        link_names=1,
+        attachments=attachments)
+
+
+def get_image_attachment():
+    items = imgur_client.get_album_images(imgur_album_id)
+    if len(items) < 1:
+        return None
+    item = random.choice(items)
+    print(item.__dir__)
+    attachments = [{
+            'fallback': "Standup!",
+            'title': "Standup!",
+            'title_link': "Standup!",
+            'image_url': item
+        }]
+    return attachments
+
+
+def post_standup():
+    message = "@here: %s" % slack_message
+    post_message(message, get_image_attachment())
 
 
 @app.route("/", methods=['POST'])
 def command():
     # ignore message we sent
-    user_name = request.form.get("user_name", "").strip()
-    print(user_name)
-    print("stuff happened")
+    incoming_text = request.form.get("text", "").strip().lower()
+
+    # find !command, but ignore <!command
+    pattern = re.compile("(!standup) ([a-zA-Z]+)")
+    match = pattern.findall(incoming_text, 0)
+
+    if not match or match[0] != "!standup":
+        print("no match")
+        print(incoming_text)
+        return
+
+    if not match[1]:
+        # do a standup
+        pass
+    elif match[1] == "help":
+        # help
+        pass
+    elif match[1] == "delay":
+        # delay
+        # return json.dumps({ })
+        pass
 
 
 @app.route("/")
 def status():
-    return 'Hello World!'
+    return "%s is running." % __name__
